86613738bba11f6328e067b376ca35dd141fb009
jturnerusa <jturner.usa@gmail.com>
2020-12-12 23:43:57 -0500

    Nightly commit: Not much progerss today. I am somewhat stuck trying to understand how the PPU works.

--- ppu.c
+++ ppu.c
@@ -8,10 +8,39 @@ void init_ppu(struct PPU *self) {
     sekf->oam_data = 0;
     self->scroll = 0;
     self->address = 0;
+    self->address_io_in_progess = 0;
     self->data = 0;
+    self->data_buffer = 0;
     self->oam_dma = 0;
     for (int i = 0; i < PPU_RAMSIZE; i++) {
         self->ram[i] = 0;
     }
     self->nmi_interrupt = 0;
+}
+
+uint8_t *access_ram(struct PPU *self, uint16_t address) {
+    return &self->ram[address];
+}
+
+void write_register_address(struct PPU *self, uint8_t data) {
+    if (self->address_write_in_progess == 0) {
+        self->address = data << 8;
+        self->address_write_in_progess = 1;
+    }
+    else {
+        self->address |= data;
+        self->address_write_in_progess = 0;
+    }
+}
+
+uint8_t read_register_data(struct PPU *self) {
+    self->data = self->data_buffer;
+    self->data_buffer = *access_ram(self, self->address);
+    self->address++;
+    return self->data;
+}
+
+void write_register_data(struct PPU *self, uint8_t data) {
+    *access_ram(self, self->address) = data;
+    self->address++;
 }
--- ppu.h
+++ ppu.h
@@ -11,11 +11,20 @@ struct PPU {
     uint8_t oam_address;
     uint8_t oam_data;
     uint8_t scroll;
-    uint8_t address;
+    uint16_t address;
+    int address_write_in_progess;
     uint8_t data;
+    uint8_t data_buffer;
     uint8_t oam_dma;
     uint8_t oam_ram[OAM_RAMSIZE]
     uint8_t ram[PPU_RAMSIZE];
     int nmi_interrupt;
+    int vblank;
+    uint64_t cycles;
+    int scanline;
+    uint16_t 16shift1;
+    uint16_t 16shift2;
+    uint8_t 8shift1;
+    uint8_t 8shift2;
 };
 #endif
