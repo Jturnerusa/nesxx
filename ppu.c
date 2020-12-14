fb662c5e33668bcdc887a6c4c297b778f615a739
jturnerusa <jturner.usa@gmail.com>
2020-12-13 23:34:04 -0500

    Nightly commit: I have a much better understanding of the PPU today, and have began working on functions to access the PPUs registers as well as sync mem mapped CPU ram to the PPU struct itself. The plan is to run the PPU cycle by cycle after each CPU instruction, hopefully it works well enough.

--- ppu.c
+++ ppu.c
@@ -7,19 +7,142 @@ void init_ppu(struct PPU *self) {
     self->oam_address = 0;
     sekf->oam_data = 0;
     self->scroll = 0;
+    self->scroll_io_in_progress;
     self->address = 0;
     self->address_io_in_progess = 0;
     self->data = 0;
     self->data_buffer = 0;
-    self->oam_dma = 0;
     for (int i = 0; i < PPU_RAMSIZE; i++) {
         self->ram[i] = 0;
     }
+    for (int i = 0; i < OAM_RAMSIZE; i++) {
+        self->oam_ram[i] = 0;
+    }
     self->nmi_interrupt = 0;
+    self->vblank = 0;
+    self->cycles = 0;
+    self->scanline = 0;
+    for (int x = 0; x < SCREEN_WIDTH; x++) {
+        for (int y = 0; y < SCREEN_HEIGHT; y++) {
+            self->frame_data[x][y] = 0;
+        }
+    }
 }
 
 uint8_t *access_ram(struct PPU *self, uint16_t address) {
     return &self->ram[address];
+}
+
+void write_controller(struct PPU *self, uint8_t value) {
+    self->controller = value;
+}
+
+uint8_t read_controller_base_nametable_address(struct PPU *self) {
+    return self->controller & 0b11;
+}
+
+int read_controller_vram_address_increment(struct PPU *self) {
+    return self->controller & 0b100;
+}
+
+int read_controller_sprite_pattern_table_address(struct PPU *self) {
+    return self->controller & 0b1000;
+}
+
+int read_controller_backround_pattern_table_address(struct PPU *self) {
+    return self->controller & 0b10000;
+}
+
+int read_controller_sprite_size(struct PPU *self) {
+    return self->controller & 0b100000;
+}
+
+int read_controller_ppu_master_slave_select(struct PPU *self) {
+    return self->controller & 0b1000000;
+}
+
+int read_controller_generate_nmi_on_vbi(struct PPU *self) {
+    return self->controller & 0b10000000;
+}
+
+void write_mask(struct PPU *self, uint8_t value) {
+    self->mask = value;
+}
+
+int read_mask_greyscale(struct PPU *self) {
+    return self->mask & 0b1;
+}
+
+int read_mask_show_leftmost_backround(struct PPU *self) {
+    return self->mask & 0b10;
+}
+
+int read_mask_show_leftmost_sprites(struct PPU *self) {
+    return self->mask & 0b100;
+}
+
+int read_mask_show_backround(struct PPU *self) {
+    return self->mask & 0b1000;
+}
+
+int read_mask_show_sprites(struct PPU *self) {
+    return self->mask & 0b10000;
+}
+
+int read_mask_emphasize_red(struct PPU *self) {
+    return self->mask & 0b00100000;
+}
+
+int read_mask_emphasize_green(struct PPU *self) {
+    return self->mask & 0b01000000;
+}
+
+int read_mask_emphasize_blue(struct PPU *self) {
+    return self->mask & 0b10000000;
+}
+
+int read_status_sprite_overflow(struct PPU *self) {
+    return self->status & 0b00100000;
+}
+
+void write_status_sprite_overflow(struct PPU *self) {
+    self->status |= 0b00100000;
+}
+
+int read_status_sprite_collision(struct PPU *self) {
+    return self->status & 0b01000000;
+}
+
+void write_status_sprite_collision(struct PPU *self) {
+    self->status |= 0b01000000;
+}
+
+int read_status_vblank_started(struct PPU *self) {
+    return self->status & 0b10000000;
+}
+
+void write_status_vblank_started(struct PPU *self) {
+    self->status |= 0b10000000;
+}
+
+void write_oam_address(struct PPU *self, uint8_t value) {
+    self->oam_address = value;
+}
+
+void write_oam_data(struct PPU *self, uint8_t value) {
+    self->oam_address++;
+    self->oam_data = value;
+}
+
+void write_scroll(struct PPU *self, uint8_t value) {
+    if (self->scroll_io_in_progress == 0) {
+        self->scroll_io_in_progress = 1;
+        self->scroll = value << 8;
+    }
+    else {
+        self->scroll_io_in_progress = 0;
+        self->scroll |= value;
+    }
 }
 
 void write_register_address(struct PPU *self, uint8_t data) {
@@ -33,14 +156,67 @@ void write_register_address(struct PPU *self, uint8_t data) {
     }
 }
 
-uint8_t read_register_data(struct PPU *self) {
-    self->data = self->data_buffer;
-    self->data_buffer = *access_ram(self, self->address);
-    self->address++;
-    return self->data;
+uint8_t read_data(struct PPU *self) {
+    if (self->address <= 0x3eff) {
+        self->data = self->data_buffer;
+        self->data_buffer = *access_ram(self, self->address);
+        self->address++;
+        return self->data;
+    }
+    else {
+        self->data = *access_ram(self, self->address);
+        self->data_buffer = *access_ram(self, self->address);
+        self->address++;
+        return self->data;        
+    }
 }
 
-void write_register_data(struct PPU *self, uint8_t data) {
+void write_data(struct PPU *self, uint8_t data) {
     *access_ram(self, self->address) = data;
     self->address++;
 }
+
+void sync_cpu_write_io(struct PPU *self, uint16_t address_accessed, uint8_t data) {
+    switch(address_accessed) {
+        case 0x2000:
+            write_controller(self, data);
+            break;
+        case 0x2001:
+            write_mask(self, data);
+            break;
+        case 0x2003:
+            write_oam_address(self, data);
+            break;
+        case 0x2004:
+            write_oam_data(self, data);
+            break;
+        case 0x2005:
+            write_scroll(self, data);
+            break;
+        case 0x2006:
+            write_address(self, data);
+            break;
+        case 0x20007:
+            write_data(self, data);
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ppu.h
+++ ppu.h
@@ -1,8 +1,19 @@
 #ifndef PPU_H
 #define PPU_H
+
+#include <stdint.h>
+
 #define PPU_RAMSIZE 0x3fff
 #define OAM_RAMSIZE 256
-#include <stdint.h>
+#define SCREEN_WIDTH
+#define SCREEN_HEIGHT
+#define PATTERN_TABLE_0 0x0000
+#define PATTERN_TABLE_1 0x1000
+#define NAMETABLE_0 0x2000
+#define NAMETABLE_1 0x2400
+#define NAMETABLE_2 0x2800
+#define NAMETABLE_3 0x2c00
+
 
 struct PPU {
     uint8_t controller;
@@ -11,20 +22,17 @@ struct PPU {
     uint8_t oam_address;
     uint8_t oam_data;
     uint8_t scroll;
+    int scroll_io_in_progress;
     uint16_t address;
     int address_write_in_progess;
     uint8_t data;
     uint8_t data_buffer;
-    uint8_t oam_dma;
-    uint8_t oam_ram[OAM_RAMSIZE]
     uint8_t ram[PPU_RAMSIZE];
+    uint8_t oam_ram[OAM_RAMSIZE]
     int nmi_interrupt;
     int vblank;
     uint64_t cycles;
     int scanline;
-    uint16_t 16shift1;
-    uint16_t 16shift2;
-    uint8_t 8shift1;
-    uint8_t 8shift2;
+    int frame_data[SCREEN_WIDTH][SCREEN_HEIGHT];
 };
 #endif
